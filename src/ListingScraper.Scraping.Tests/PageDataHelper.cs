7ccd4efbe8628cd07995e1ed34d221f1044da1ef
amjed <amjed1@live.com>
2018-10-12 22:34:08 +0100

    fix directory seperator in test

--- src/ListingScraper.Scraping.Tests/PageDataHelper.cs
+++ src/ListingScraper.Scraping.Tests/PageDataHelper.cs
@@ -1,15 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
 using ListingScraper.Entities.Enums;
 
 namespace ListingScraper.Scraping.Tests
 {
     public static class PageDataHelper
     {
+        private static char DirectorySeperator = RuntimeInformation.IsOSPlatform(OSPlatform.Linux)? '/' : '\\';
         public static string GetListingPage(Source source, int page)
         {
             var filename = $"{page}.html";
-            var dir = $"Pages\\{source}\\Listings";
-            var path = $"{dir}\\{filename}";
+            var dir = $"Pages{DirectorySeperator}{source}{DirectorySeperator}Listings";
+            var path = $"{dir}{DirectorySeperator}{filename}";
             if (File.Exists(path) == false)
                 throw new FileNotFoundException($"page test data file {filename}not found");
 
@@ -20,8 +23,8 @@ namespace ListingScraper.Scraping.Tests
         public static string GetListingItemPage(Source source, int page)
         {
             var filename = $"{page}.html";
-            var dir = $"Pages\\{source}\\Items";
-            var path = $"{dir}\\{filename}";
+            var dir = $"Pages{DirectorySeperator}{source}{DirectorySeperator}Items";
+            var path = $"{dir}{DirectorySeperator}{filename}";
             if (File.Exists(path) == false)
                 throw new FileNotFoundException($"page test data file {filename}not found");
 
